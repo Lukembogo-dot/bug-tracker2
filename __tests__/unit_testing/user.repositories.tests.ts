bec3c82c7bfeeeed18e4e8173818eb3f4676a6a2
Storm00212 <paulmuyalikhams@gmail.com>
2025-10-30 16:30:45 +0300

    user repositories test file created

---
+++ __tests__/unit_testing/user.repositories.tests.ts
@@ -0,0 +1,12 @@
+jest.mock('../../src/repositories/user.repositories.ts');
+jest.mock('../../src/services/user.services.ts');
+jest.mock("bcrypt");
+jest.mock("jsonwebtoken");
+jest.mock("Request")
+jest.mock("Response")
+jest.mock("Pool");
+
+describe("User service testing", () => {
+    
+})
+
