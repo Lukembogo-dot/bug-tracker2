9dee11d2cb85469299ed2fc3e7ffca8e38319f7c
Luke <lukembogo5@gmail.com>
2025-11-12 10:06:13 +0300

    comment test

--- __tests__/unit_testing/comments.repositories.test.ts
+++ __tests__/unit_testing/comments.repositories.test.ts
@@ -1,1 +1,22 @@
-jest.mock("../../src/repositories/comments.repositorys", () =>)
+import { CommentRepository } from "../../src/repositories/comments.repositories"
+
+
+    describe("CommentRepository Mock", () => {
+         test("comments can be retreived from the database", async () => {
+            const mockComment ={
+                            CommentID: 1,
+                            BugID: 1,
+                            UserID: 1,
+                            CommentText: "This is a comment",
+                            CreatedAt: new Date(),
+                            UpdatedAt: new Date()
+    };
+
+        jest.spyOn(CommentRepository, "getCommentById").mockResolvedValueOnce(mockComment);
+
+        const comments = await CommentRepository.getCommentById(1);
+
+        expect(comments).toEqual(mockComment);
+
+         });
+    });
