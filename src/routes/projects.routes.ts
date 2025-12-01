8aa654ff897f883c9d99a4364737e6291258c244
Luke <lukembogo5@gmail.com>
2025-12-01 15:25:58 +0300

    added /api to projects

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -4,22 +4,22 @@ import { requireAuth } from "../middleware/auth.middleware";
 
 const projectRoutes = (app:Express) => {
     // GET /projects - Retrieve all projects
-    app.get('/projects', requireAuth, projectController.getAllProjectsController);
+    app.get('/api/projects', requireAuth, projectController.getAllProjectsController);
 
     // GET /projects/:id - Retrieve a specific project by ID
-    app.get('/projects/:id', requireAuth, projectController.getProjectByIdController);
+    app.get('/api/projects/:id', requireAuth, projectController.getProjectByIdController);
 
     // GET /projects/creator/:creatorId - Retrieve projects by creator
-    app.get('/projects/creator/:creatorId', requireAuth, projectController.getProjectsByCreatorController);
+    app.get('/api/projects/creator/:creatorId', requireAuth, projectController.getProjectsByCreatorController);
 
     // POST /projects - Create a new project
-    app.post('/projects', requireAuth, projectController.createProjectController);
+    app.post('/api/projects', requireAuth, projectController.createProjectController);
 
     // PUT /projects/:id - Update an existing project
-    app.put('/projects/:id', requireAuth, projectController.updateProjectController);
+    app.put('/api/projects/:id', requireAuth, projectController.updateProjectController);
 
     // DELETE /projects/:id - Delete a project
-    app.delete('/projects/:id', requireAuth, projectController.deleteProjectController);
+    app.delete('/api/projects/:id', requireAuth, projectController.deleteProjectController);
 }
 
 export default projectRoutes;
